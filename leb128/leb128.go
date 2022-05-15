860da04ba3763367f5103a7edbe71de34b5a6b43
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2022-05-15 18:18:20 -0400

    chore: implementing leb128 encoding and decoding

--- leb128/leb128.go
+++ leb128/leb128.go
@@ -1,14 +1,111 @@
 package leb128
 
-type Encoded byte
-
-const (
-	Signed Encoded = iota
-	Unsigned
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"unsafe"
 )
 
-func Encode(n []byte, ttype Encoded) {
+var (
+	ErrCannotReadNextByte = errors.New("cannot read next byte")
+	ErrOverflow32         = errors.New("overflows a 32-bit integer")
 
+	// cachedLEB128Encoded goes from 0 -> 127 since the LEB128 is the number
+	cachedLEB128Encoded = [0x80][1]byte{
+		{0x00}, {0x01}, {0x02}, {0x03}, {0x04}, {0x05}, {0x06}, {0x07}, {0x08}, {0x09}, {0x0a}, {0x0b}, {0x0c}, {0x0d}, {0x0e}, {0x0f},
+		{0x10}, {0x11}, {0x12}, {0x13}, {0x14}, {0x15}, {0x16}, {0x17}, {0x18}, {0x19}, {0x1a}, {0x1b}, {0x1c}, {0x1d}, {0x1e}, {0x1f},
+		{0x20}, {0x21}, {0x22}, {0x23}, {0x24}, {0x25}, {0x26}, {0x27}, {0x28}, {0x29}, {0x2a}, {0x2b}, {0x2c}, {0x2d}, {0x2e}, {0x2f},
+		{0x30}, {0x31}, {0x32}, {0x33}, {0x34}, {0x35}, {0x36}, {0x37}, {0x38}, {0x39}, {0x3a}, {0x3b}, {0x3c}, {0x3d}, {0x3e}, {0x3f},
+		{0x40}, {0x41}, {0x42}, {0x43}, {0x44}, {0x45}, {0x46}, {0x47}, {0x48}, {0x49}, {0x4a}, {0x4b}, {0x4c}, {0x4d}, {0x4e}, {0x4f},
+		{0x50}, {0x51}, {0x52}, {0x53}, {0x54}, {0x55}, {0x56}, {0x57}, {0x58}, {0x59}, {0x5a}, {0x5b}, {0x5c}, {0x5d}, {0x5e}, {0x5f},
+		{0x60}, {0x61}, {0x62}, {0x63}, {0x64}, {0x65}, {0x66}, {0x67}, {0x68}, {0x69}, {0x6a}, {0x6b}, {0x6c}, {0x6d}, {0x6e}, {0x6f},
+		{0x70}, {0x71}, {0x72}, {0x73}, {0x74}, {0x75}, {0x76}, {0x77}, {0x78}, {0x79}, {0x7a}, {0x7b}, {0x7c}, {0x7d}, {0x7e}, {0x7f},
+	}
+)
+
+func EncodeUint(v uint) (enc []byte) {
+	if v < 0x80 {
+		return cachedLEB128Encoded[v][:]
+	}
+
+	for v != 0 {
+		b := v & 0x7f
+		v >>= 7
+
+		if v != 0 {
+			b |= 0x80
+		}
+
+		enc = append(enc, byte(b))
+	}
+
+	return enc
 }
 
-func encodeUnsigned()
+func EncodeInt(v int) (enc []byte) {
+	for {
+		b := v & 0x7f
+		s := v & 0x40
+
+		v >>= 7
+
+		if (v != -1 || s == 0) && (v != 0 || s != 0) {
+			b |= 0x80
+		}
+
+		enc = append(enc, byte(b))
+		if b&0x80 == 0 {
+			break
+		}
+	}
+
+	return enc
+}
+
+func DecodeUint(enc []byte) (result uint, err error) {
+	reader := bytes.NewReader(enc)
+	shift := 0
+
+	for {
+		b, err := reader.ReadByte()
+		if err != nil {
+			return result, fmt.Errorf("%w: %s", ErrCannotReadNextByte, err.Error())
+		}
+
+		result |= (uint(b&0x7f) << shift)
+
+		if (b & 0x80) == 0 {
+			break
+		}
+
+		shift += 7
+	}
+
+	return result, nil
+}
+
+func DecodeInt[T int32 | int64](enc []byte) (result T, err error) {
+	reader := bytes.NewReader(enc)
+	shift := 0
+
+	for {
+		b, err := reader.ReadByte()
+		if err != nil {
+			return result, fmt.Errorf("%w: %s", ErrCannotReadNextByte, err.Error())
+		}
+
+		result |= T(b&0x7f) << shift
+		shift += 7
+
+		if b&0x80 == 0 {
+			if shift < int(unsafe.Sizeof(result)*8) && (b&0x40) != 0 {
+				result |= ^0 << shift
+			}
+
+			break
+		}
+	}
+
+	return result, nil
+}
--- leb128/leb128_test.go
+++ leb128/leb128_test.go
@@ -1,23 +1,142 @@
 package leb128_test
 
 import (
-	"encoding/binary"
+	"math"
 	"testing"
 
 	"github.com/EclesioMeloJunior/gowasm/leb128"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestUnsignedLEB128(t *testing.T) {
+func TestEncodeInt(t *testing.T) {
 	tests := []struct {
-		number   uint32
+		input    int
 		expected []byte
 	}{
-		{624485, []byte{0xE5, 0x8E, 0x26}},
+		{input: -math.MaxInt32, expected: []byte{0x81, 0x80, 0x80, 0x80, 0x78}},
+		{input: -165675008, expected: []byte{0x80, 0x80, 0x80, 0xb1, 0x7f}},
+		{input: -624485, expected: []byte{0x9b, 0xf1, 0x59}},
+		{input: -16256, expected: []byte{0x80, 0x81, 0x7f}},
+		{input: -4, expected: []byte{0x7c}},
+		{input: -1, expected: []byte{0x7f}},
+		{input: 0, expected: []byte{0x00}},
+		{input: 1, expected: []byte{0x01}},
+		{input: 4, expected: []byte{0x04}},
+		{input: 16256, expected: []byte{0x80, 0xff, 0x0}},
+		{input: 624485, expected: []byte{0xe5, 0x8e, 0x26}},
+		{input: 165675008, expected: []byte{0x80, 0x80, 0x80, 0xcf, 0x0}},
+		{input: math.MaxInt32, expected: []byte{0xff, 0xff, 0xff, 0xff, 0x7}},
+		{input: math.MaxInt64, expected: []byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x0}},
 	}
 
 	for _, tt := range tests {
-		var 
-		binary.BigEndian.PutUint32(, tt.number)
-		leb128.Encode()
+		enc := leb128.EncodeInt(tt.input)
+		require.Equal(t, tt.expected, enc)
+	}
+}
+
+func TestEncodeUint(t *testing.T) {
+	tests := []struct {
+		input    uint
+		expected []byte
+	}{
+		{input: 0, expected: []byte{0x00}},
+		{input: 1, expected: []byte{0x01}},
+		{input: 4, expected: []byte{0x04}},
+		{input: 16256, expected: []byte{0x80, 0x7f}},
+		{input: 624485, expected: []byte{0xe5, 0x8e, 0x26}},
+		{input: 165675008, expected: []byte{0x80, 0x80, 0x80, 0x4f}},
+		{input: math.MaxUint32, expected: []byte{0xff, 0xff, 0xff, 0xff, 0xf}},
+		{input: math.MaxUint64, expected: []byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x1}},
+	}
+
+	for _, tt := range tests {
+		enc := leb128.EncodeUint(tt.input)
+		require.Equal(t, tt.expected, enc)
+	}
+}
+
+func TestDecodeUint(t *testing.T) {
+	tests := []struct {
+		enc      []byte
+		expected uint
+		wantErr  error
+	}{
+		{enc: []byte{0x00}, expected: 0},
+		{enc: []byte{0x04}, expected: 4},
+		{enc: []byte{0x01}, expected: 1},
+		{enc: []byte{0x80, 0}, expected: 0},
+		{enc: []byte{0x80, 0x7f}, expected: 16256},
+		{enc: []byte{0xe5, 0x8e, 0x26}, expected: 624485},
+		{enc: []byte{0x80, 0x80, 0x80, 0x4f}, expected: 165675008},
+		{enc: []byte{0xff, 0xff, 0xff, 0xff, 0xf}, expected: 0xffffffff},
+		{enc: []byte{0xff, 0xff, 0xff, 0xff, 0xf}, expected: math.MaxUint32},
+		{enc: []byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x1}, expected: math.MaxUint64},
+	}
+
+	for _, tt := range tests {
+		result, err := leb128.DecodeUint(tt.enc)
+		if tt.wantErr != nil {
+			assert.EqualError(t, err, tt.wantErr.Error())
+		} else {
+			assert.NoError(t, err)
+			assert.Equal(t, tt.expected, result)
+		}
+	}
+}
+
+func TestDecodeInt32(t *testing.T) {
+	tests := []struct {
+		enc      []byte
+		expected int32
+		wantErr  error
+	}{
+		{enc: []byte{0x13}, expected: 19},
+		{enc: []byte{0x00}, expected: 0},
+		{enc: []byte{0x04}, expected: 4},
+		{enc: []byte{0xFF, 0x00}, expected: 127},
+		{enc: []byte{0x81, 0x01}, expected: 129},
+		{enc: []byte{0x7f}, expected: -1},
+		{enc: []byte{0x81, 0x7f}, expected: -127},
+		{enc: []byte{0xFF, 0x7e}, expected: -129},
+	}
+
+	for _, tt := range tests {
+		result, err := leb128.DecodeInt[int32](tt.enc)
+		if tt.wantErr != nil {
+			assert.EqualError(t, err, tt.wantErr.Error())
+		} else {
+			assert.NoError(t, err)
+			assert.Equal(t, tt.expected, result)
+		}
+	}
+}
+
+func TestDecodeInt64(t *testing.T) {
+	tests := []struct {
+		enc      []byte
+		expected int64
+		wantErr  error
+	}{
+		{enc: []byte{0x00}, expected: 0},
+		{enc: []byte{0x04}, expected: 4},
+		{enc: []byte{0xFF, 0x00}, expected: 127},
+		{enc: []byte{0x81, 0x01}, expected: 129},
+		{enc: []byte{0x7f}, expected: -1},
+		{enc: []byte{0x81, 0x7f}, expected: -127},
+		{enc: []byte{0xFF, 0x7e}, expected: -129},
+		{enc: []byte{0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x7f},
+			expected: -9223372036854775808},
+	}
+
+	for _, tt := range tests {
+		result, err := leb128.DecodeInt[int64](tt.enc)
+		if tt.wantErr != nil {
+			assert.EqualError(t, err, tt.wantErr.Error())
+		} else {
+			assert.NoError(t, err)
+			assert.Equal(t, tt.expected, result)
+		}
 	}
 }
